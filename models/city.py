e6d952ac6c78fd20530744fa226f1290693c8900
ALASHI1 <alashimuyiwa@gmail.com>
2022-09-03 20:54:44 +0100

    tests

---
+++ models/city.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module provides the `City` class
+"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """Defines a `City` class"""
+
+    state_id = ""
+    name = ""
